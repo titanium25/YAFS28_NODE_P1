08dded21fc8ef4de678a69f7c672e2127afb59c7
titanium25 <idpm7N95tLPLskg>
2021-06-12 10:12:29 +0300

    add movie id start from last API id

--- model/moviesBL.js
+++ model/moviesBL.js
@@ -3,20 +3,30 @@ const jsonDAL = require('../DAL/jsonDAL')
 
 exports.findMovie = async function(req) {
     const movies = await restDAL.getAllMovies()
+    const moviesJSON = await jsonDAL.getMovies()
     const title = req.body.title.toLowerCase();
     const genre = req.body.genres;
     const language = req.body.language;
-    return movies.data.filter(item =>
-        (item.name.toLowerCase().includes(title) || title == '') &&
+    // let movieREST = movies.data.filter(item =>
+    //     (item.name.toLowerCase().includes(title) || title == '') &&
+    //     (item.language == language || language == '') &&
+    //     (item.genres.includes(genre) || genre == '')
+    // )
+
+    return moviesJSON.movies.filter(item =>
+        (item.title.toLowerCase().includes(title) || title == '') &&
         (item.language == language || language == '') &&
-        (item.genres.includes(genre) || genre == '')
+        (item.genre.includes(genre) || genre == '')
     )
+
+    // return movieREST.concat(movieJSON)
 }
 
 exports.addMovie = async function(req) {
     const title = req.body.title;
     const genre = req.body.genres;
     const language = req.body.language;
+
     if(title.length == 0 ) {
         return "Please type movie title"
     }
@@ -27,9 +37,14 @@ exports.addMovie = async function(req) {
         return "Please choose movie genre"
     } else {
         let moviesArr = await jsonDAL.getMovies();
+        const moviesAPI = await restDAL.getAllMovies()
+        let last = moviesAPI.data
+
         let movies = moviesArr.movies
         let lastMovie = movies[movies.length - 1]
+        if(lastMovie === undefined) lastMovie = last[last.length - 1]
         let id = lastMovie.id + 1
+
         movies.push({
             id,
             title,
